4e2176071572214ec0a91a3aaf05893bdd3fdaf8
Richard Sandoval <risandoval@contraloria.gob.do>
2015-12-07 15:55:00 -0400

    before something

--- public/js/controllers/chart.js
+++ public/js/controllers/chart.js
@@ -8,20 +8,8 @@ app
     .controller('FlotChartDemoCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
 
 
-        //$scope.bw = $http.get(app.api + '/analysis/bw?uname=' + $window.data.user + '&start=' + (new Date().getDate() - 1 ) + '&ends=' + (new Date().getDate() + 1 ) + '&criteria=min', {
-        //    Bearer: $window.data.token,
-        //    uname: $window.data.user
-        //}).then(function (response) {
-        //    console.log(app.api + '/analysis/bw?uname=' + $window.data.user + '&start=' + (new Date().getDate() - 1 ) + '&ends=' + (new Date().getDate() + 1 ) + '&criteria=min');
-        //    console.log(response);
-        //    //return response;
-        //}, function (err) {
-        //    console.log(err);
-        //    console.log(app.api + '/analysis/bw?uname=' + $window.data.user + '&start=' + (new Date().getDate() - 1 ) + '&ends=' + (new Date().getDate() + 1 ) + '&criteria=min');
-        //});
 
         $scope.d0_1 = [[1, 6.5], [2, 6.5], [3, 7], [4, 8], [5, 7.5], [6, 7], [7, 6.8], [8, 7], [9, 7.2], [10, 7], [11, 6.8], [12, 7]];
-        var j = 0;
         var result = [];
         $scope.d0_2 = [0, 4], [1, 4.5], [2, 7], [3, 4.5], [4, 3], [5, 3.5], [6, 6], [7, 3], [8, 4], [9, 3];
 
@@ -35,8 +23,13 @@ app
         $scope.d1_2 = [[10, 50], [20, 60], [30, 90], [40, 35]];
 
         $scope.d1_3 = [[10, 80], [20, 40], [30, 30], [40, 20]];
-
+        $scope.sizePacket = 0;
         $scope.d2 = [];
+        $scope.time = {
+            ago : new Date()
+        };
+
+        //$scope.ago = new Date('2015/06/15');
 
         for (var i = 0; i < 20; ++i) {
             $scope.d2.push([i, Math.round(Math.sin(i) * 100) / 100]);
@@ -50,6 +43,8 @@ app
             {label: "iPad Air", data: 18}
         ];
 
+
+
         $scope.refreshData = function (which) {
             var k = 0;
             result = [];
@@ -62,25 +57,28 @@ app
                 var labels = [];
                 var datas = [];
                 $scope.data = [[]];
+                var time = new Date( res.data.bw[res.data.bw.length -1].time);
+                $scope.sizePacket = res.data.bw.length;
                 res.data.bw.forEach(function (data) {
-                    var arr = new Array(2);
-                    arr[1] = data.dataUse;
-                    arr[0] = k++;
-                    var time = new Date(data.time);
+                    //var arr = new Array(2);
+                    //arr[1] = data.dataUse;
+                    //arr[0] = k++;
+                    time = new Date(data.time);
                     labels.push(time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds());
                     datas.push(data.dataUse);
-                    result.push(arr);
+
+                    //result.push(arr);
                 });
                 //console.log(result);
                 $scope.data.push(sampling(datas));
-
+                $scope.time.ago.setDate(time.getDate());
                 $scope.labels = (sampling(labels));
                 //$scope.label = result[0];
                 //$scope.data = result[1];
                 //$scope.bw = result;
             }, function (err) {
                 console.log(err);
-                $scope.bw = $scope.d0_1;
+                //$scope.bw = $scope.d0_1;
             });
 
         };
@@ -94,12 +92,12 @@ app
             }).then(function (res) {
                 $scope.pieData = [];
                 $scope.pieLabel = [];
-                var p = [],d = [];
-                var pieD = res.data.filter(function(f){
+                var p = [], d = [];
+                var pieD = res.data.filter(function (f) {
                     return f.criteria < 1024;
                 });
 
-                pieD.forEach(function(data){
+                pieD.forEach(function (data) {
                     d.push(data.count);
                     p.push(data.criteria);
                     console.log(data.criteria);
@@ -113,8 +111,36 @@ app
             });
         };
 
-        $scope.topDest = function(){
+        $scope.topDest = function () {
+            $http.get(app.api + '/analysis/rank?uname=' + $window.data.user + '&start=' + (new Date() - 20) + '&ends=' + (new Date() + 1 ) + '&criteria=dIP', {
+                headers: {
+                    Bearer: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NjM5MTU2ZGY3NjMwYzI4MjFhOWQ3Y2QiLCJ1c2VyIjoicnNhbmRvdmFsIiwicHdyIjoicnNhbmRvdmFsIiwic3RhZmZJZCI6IjU2M2ExN2E5Njk3OGIyMTE0ZWQzYzJkYiJ9.p6z5FZjvnnlbpXNy9OTTqqSJfwy7KDerR_HMdoPTAi4',
+                    uname: 'rsandoval'
+                }
+            }).then(function (res) {
+                $scope.topDestData = [[]];
+                $scope.topDestLabels = [];
 
+                var topD = res.data;
+                var labels = [];
+                var datas = [];
+                if(topD.length > 5){
+                    for(var i = 0 ; i  < 5 ; i++){
+                        var data = topD[i];
+                        datas.push(data.count);
+                        labels.push(data.criteria);
+                    }
+                }else{
+                    topD.forEach(function(data){
+                        datas.push(data.count);
+                        labels.push(data.criteria);
+                    });
+                }
+                $scope.topDestData= datas;
+                $scope.topDestLabels = labels;
+            }, function (err) {
+                console.log(err);
+            });
         };
 
         var sampling = function (array) {
@@ -132,7 +158,7 @@ app
             }
         };
 
-        $scope.topDestData = [];
+        $scope.topDestData = [[]];
         $scope.topDestLabels = [];
         $scope.pieData = [];
         $scope.pieLabel = [];
